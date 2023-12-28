f29b3d77d2f150055fc79cee4d60e64de764939c
SebasMG0 <s.murciag@uniandes.edu.co>
2023-12-27 19:05:32 -0500

    Eliminado UrbanStyle. Los proyectos tendrán sus propios repositorios

--- UrbanStyle/home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns= [
-    path('', views.welcomeMessage, name="home"),
-    path('message/<str:name>', views.showMessage, name= "message"),
-]
